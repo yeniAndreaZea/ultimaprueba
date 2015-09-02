bd6c25c7ec170e44c58d2541cb7fcdc0afbb7cfc
yeniAndreaZea <makiu0520@gmail.com>
2015-09-02 16:19:40 -0500

    he terminado

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTP-8">
+  <title> document</title>
+</head>
+<body>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+<?php echo "hola yeni";?>
+</body>
+</html>
---
+++ index2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTP-8">
+  <title> document</title>
+</head>
+<body>
+<?php echo "hola mundo";?>
+<?php echo "hola mundo";?>
+<?php echo "hola mundo";?>
+<?php echo "hola mundo";?>
+<?php echo "hola mundo";?>
+
+</body>
+</html>
